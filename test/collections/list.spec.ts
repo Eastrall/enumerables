eea7f3898463a1db080ce8f22affd138e49a0b0d
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2019-08-12 11:39:31 +0200

    Add more list unit test

--- test/collections/list.spec.ts
+++ test/collections/list.spec.ts
@@ -1,4 +1,5 @@
 import { List, Enumerable } from '../../src/enumerables';
+import { Func2 } from '../../src/internal/types';
 
 interface Person {
     name: string;
@@ -78,6 +79,100 @@ describe('List', () => {
         expect(list.count()).toEqual(10);
         list.clear();
         expect(list.count()).toEqual(0);
+    });
+
+    describe('aggregate', () => {
+        // This is the test case from 
+        // https://docs.microsoft.com/fr-fr/dotnet/api/system.linq.enumerable.aggregate?view=netcore-2.2#System_Linq_Enumerable_Aggregate__1_System_Collections_Generic_IEnumerable___0__System_Func___0___0___0__
+        it('should apply an accumulator over a string list.', () => {
+            const sentence: string = 'the quick brown fox jumps over the lazy dog';
+            const wordsArray: Array<string> = sentence.split(' ');
+            const words: List<string> = new List<string>(wordsArray);
+
+            const reversedSentence: string = words.aggregate((workingSentence, next) => next + ' ' + workingSentence);
+
+            expect(reversedSentence).toEqual('dog lazy the over jumps fox brown quick the');
+            expect(reversedSentence).toEqual(wordsArray.reverse().join(' '));
+        });
+
+        // This is the test case from
+        // https://docs.microsoft.com/fr-fr/dotnet/api/system.linq.enumerable.aggregate?view=netcore-2.2#System_Linq_Enumerable_Aggregate__2_System_Collections_Generic_IEnumerable___0____1_System_Func___1___0___1__
+        it('should apply an accumulator over a list using a seed value.', () => {
+            const numbers: Array<number> = [4, 8, 8, 3, 9, 0, 7, 8, 2];
+            const numbersList: List<number> = new List<number>(numbers);
+
+            const evenNumbers: number = numbersList.aggregate(0, (total, next) => next % 2 === 0 ? total + 1 : total);
+
+            expect(evenNumbers).toEqual(6);
+        });
+
+        // This is the test case from
+        // https://docs.microsoft.com/fr-fr/dotnet/api/system.linq.enumerable.aggregate?view=netcore-2.2#System_Linq_Enumerable_Aggregate__3_System_Collections_Generic_IEnumerable___0____1_System_Func___1___0___1__System_Func___1___2__
+        it('should apply an accumulator over a list using a seed value and result selector.', () => {
+            const fruits: Array<string> = ['apple', 'mango', 'orange', 'passionfruit', 'grape'];
+            const fruitsList: List<string> = new List<string>(fruits);
+
+            const longestFruitName: string = fruitsList.aggregate('banana', (longest, next) => next.length > longest.length ? next : longest, x => x.toUpperCase());
+        
+            expect(longestFruitName).toEqual('PASSIONFRUIT');
+        });
+
+        it('should throw an error if the list is empty.', () => {
+            const list: List<any> = new List<any>();
+
+            expect(() => list.aggregate(x => x)).toThrow();
+        });
+
+        it('should throw an error if the accumulator function is undefined.', () => {
+            const numbers: Array<number> = [4, 8, 8, 3, 9, 0, 7, 8, 2];
+            const numbersList: List<number> = new List<number>(numbers);
+            const accumulatorFunction: Func2<number, number, number> = undefined;
+
+            expect(() => numbersList.aggregate(accumulatorFunction)).toThrow();
+            expect(() => numbersList.aggregate(0, accumulatorFunction)).toThrow();
+            expect(() => numbersList.aggregate(0, accumulatorFunction, x => x)).toThrow();
+        });
+    });
+
+    describe('all', () => {
+        const list: List<Person> = new List<Person>(arrayOfPersons);
+        
+        it('should throw an error if the matching predicate is undefined.', () => {
+            const list: List<any> = new List<any>();
+
+            expect(() => list.all(undefined)).toThrow();
+        });
+
+        it('should match every elements of array.', () => {
+            expect(list.all(x => x.age < 100)).toEqual(true);
+        });
+        
+        it('should not match every elements of array.', () => {
+            expect(list.all(x => x.name.startsWith('John'))).toEqual(false);
+        });
+    });
+
+    describe('any', () => {
+        const list: List<Person> = new List<Person>(arrayOfPersons); 
+
+        it('should return true if list contains elements.', () => {
+            expect(list.any()).toBeTruthy();
+            expect(list.any()).toEqual(true);
+        });
+
+        it('should return false if list doesnt contains elements.', () => {
+            const emptyList: List<any> = new List<any>();
+
+            expect(emptyList.any()).toEqual(false);
+        });
+
+        it('should return true if list contains elements matching a predicate function.', () => {
+            expect(list.any(x => x.name.startsWith('J'))).toEqual(true);
+        });
+
+        it('should return false if list doesn\'t elements matching a predicate function.', () => {
+            expect(list.any(x => x.age === 150)).toEqual(false);
+        });
     });
 
     describe('count', () => {
@@ -455,6 +550,17 @@ describe('List', () => {
             expect(list.indexOf(undefined)).toEqual(-1);
             expect(list.indexOf(-1)).toEqual(-1);
             expect(list.indexOf(12)).toEqual(-1);
+        });
+    });
+
+    describe('insert', () => {
+        it('should insert an element at a given position.', () => {
+            const list: List<Person> = new List<Person>(arrayOfPersons);
+
+            list.insert(1, { name: 'NewPerson', age: 42 });
+
+            expect(list.count()).toEqual(arrayOfPersons.length + 1);
+            expect(list.elementAt(1)).toEqual({ name: 'NewPerson', age: 42 });
         });
     });
 
